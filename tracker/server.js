3a62d029781f01f04bef955472259bd46f67e9dd
Guillaume Petitpierre <gpetitpierre@infinit8.io>
2016-08-22 14:02:28 +0200

    WebSocket tracker

---
+++ tracker/server.js
@@ -0,0 +1,28 @@
+var Server = require('bittorrent-tracker').Server
+
+var server = new Server({
+  udp: false,
+  http: false,
+  ws: true,
+  stats: true
+})
+
+server.on('error', function(err){
+  console.log(err.message)
+})
+
+server.on('warning', function(err){
+  console.log(err.message)
+})
+
+server.on('listening', function(){
+  console.log('listening on ws port: '+ server.ws.address().port)
+})
+
+server.listen(8998, 'localhost', function (){
+  console.log("starting")
+})
+
+server.on('start', function(addr) {
+  console.log('got start message from ' + addr)
+})
