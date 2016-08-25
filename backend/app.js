1d1beeff96a4d357a4ab763f09ad361adcc93f52
Diogo-Ferreira <d.venancio.ferreira@gmail.com>
2016-08-25 15:08:59 +0200

    create and explore endpoint

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
-require('./models')
+var data = require('./data')
+var url = 'mongodb://localhost:27017/mongo-node-backend';
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+data.init(url);
 
 app.get('/', function (req, res){
    res.send("Hello world !");
@@ -8,16 +12,30 @@ app.get('/', function (req, res){
 
 
 app.get('/explore',function(req,res){
-
+  data.find(data.db,{},'rooms',function(rooms){
+    res.json(rooms);
+  });
 });
 
 
-app.get('/create',function(req,res){
-
+app.post('/create',function(req,res){
+  //Still need to add params validation
+  data.insert(data.db,'rooms',{
+    "torren_magnet_link" : req.body.torren_magnet_link,
+    "joignable_after_start" : req.body.joignable_after_start,
+    "name"  : req.body.name,
+    "admin" : req.body.admin,
+    "private" : req.body.private,
+    "max_spectators" : req.body.max_spectators,
+    "description" : req.body.description
+  },function (result){
+    res.send(result)
+  });
 });
 
 
 app.get('/watch',function(req,res){
+  //Send magnet link and admin peerid
 
 });
 
---
+++ backend/data.js
@@ -0,0 +1,33 @@
+
+var assert = require('assert');
+var MongoClient = require('mongodb').MongoClient
+
+var init = function(url){
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected succesfully to server");
+    module.exports.db = db;
+  });
+}
+
+
+
+var find = function(db,attributes,collectionName,callback){
+  var collection = db.collection(collectionName);
+  collection.find(attributes).toArray(function(err,result){
+    assert.equal(err, null);
+    callback(result);
+  });
+}
+
+var insert = function(db,collectionName,collectionData,callback){
+  var collection = db.collection(collectionName);
+  collection.insertOne(collectionData, function(err, result) {
+    assert.equal(err, null);
+    callback(result);
+  });
+}
+
+module.exports.insert = insert;
+module.exports.find = find;
+module.exports.init = init;
